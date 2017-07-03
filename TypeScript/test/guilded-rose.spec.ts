e72b12ca4746a3f439cda8353c4d6ac3a227f665
Paulo Clavijo Esteban <paulo.esteban@emc.com>
2017-07-03 16:07:18 +0100

    Added starting kata code for TypeScript language.

---
+++ TypeScript/test/guilded-rose.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { Item } from '../app/item';
+import { GildedRose } from '../app/gilded-rose';
+
+describe('Gilded Rose', function () {
+
+    it('should foo', function() {
+        const gilgedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+        const items = gilgedRose.updateQuality();
+        expect(items[0].name).to.equal('fixme');
+    });
+
+});
