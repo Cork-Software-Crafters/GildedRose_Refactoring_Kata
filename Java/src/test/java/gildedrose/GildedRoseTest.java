e50ba6140994355bc72b4d2ec4c69abf725dcbc9
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-25 13:43:21 +0100

    Adding more scenarios to get 100% coverage

--- Java/src/test/java/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/gildedrose/GildedRoseTest.java
@@ -10,8 +10,8 @@ public class GildedRoseTest {
     @Test
     public void should_update_quality_of_an_item() {
         String[] names = {"an item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-        Integer[] sellIns = {0};
-        Integer[] qualities = {0, 1};
+        Integer[] sellIns = {-1, 0, 11};
+        Integer[] qualities = {0, 1, 50, 49};
 
         CombinationApprovals.verifyAllCombinations(
                 this::updateQuality,
