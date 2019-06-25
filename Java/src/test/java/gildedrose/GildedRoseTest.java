840d54b03b7104a6f3b1c4f17d1b48d4b2a16224
Paulo Clavijo Esteban <paucls@gmail.com>
2019-06-25 13:37:56 +0100

    Refactor introducing explanatory variables

--- Java/src/test/java/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/gildedrose/GildedRoseTest.java
@@ -9,11 +9,15 @@ public class GildedRoseTest {
 
     @Test
     public void should_update_quality_of_an_item() {
+        String[] names = {"an item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] sellIns = {0};
+        Integer[] qualities = {0, 1};
+
         CombinationApprovals.verifyAllCombinations(
                 this::updateQuality,
-                new String[]{"an item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
-                new Integer[]{0},
-                new Integer[]{0, 1});
+                names,
+                sellIns,
+                qualities);
     }
 
     private String updateQuality(String name, int sellIn, int quality) {
